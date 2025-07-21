21ee5867d61130497ff0798bb5275a0c7fd8575a
Justin Crocker <justincrocker@gmail.com~>
2025-07-21 11:44:22 -0230

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div className="p-8">
+      <h1 className="text-4xl font-bold text-blue-600">Tailwind is working!</h1>
+      <p className="mt-4 text-gray-600">Let’s build something cool.</p>
+    </div>
+  );
+}
