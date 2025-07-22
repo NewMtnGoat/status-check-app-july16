e638a49e1e540d6aa9a95b501b2c80836c510f3f
NewMtnGoat <justincrocker@gmail.com>
2025-07-22 17:16:54 -0230

    implement changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import React from "react";
-import Auth from "./Auth";
+// src/main.jsx
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx' // 👈 Make sure this exists
+import './index.css'
 
-function App() {
-  return (
-    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-      <Auth />
-    </div>
-  );
-}
-
-export default App;
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
